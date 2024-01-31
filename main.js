29d97212ec6725731bcd3149725aea3e1c115953
vsyamini <116477617+vsyamini@users.noreply.github.com>
2024-01-31 20:32:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Python Developer", "Web Developer using Django"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
